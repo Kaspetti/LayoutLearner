fcc498eb4d1c6e8712cae581e4ccc0f0cd7bb6eb
Kaspar Moberg <kaspeti123@gmail.com>
2024-01-01 01:04:18 +0100

    added word generation

--- cmd/LayoutLearner/main.go
+++ cmd/LayoutLearner/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/Kaspetti/LayoutLearner/internal/dictionary"
 	"github.com/gdamore/tcell/v2"
@@ -9,53 +11,56 @@ import (
 )
 
 
-const testString = "aion nio aia eano nona anio"
-
-
 func main() {
+    rand.Seed(time.Now().UTC().UnixNano())
+
     characterPriority, err := dictionary.GetCharacterPriority("resources/words.txt")
     if err != nil {
         panic(err)
     }
-    _ = characterPriority
+
+    words, colorMap := newGame(characterPriority)
 
     app := tview.NewApplication()
     textView := tview.NewTextView().
         SetDynamicColors(true).
         SetRegions(true)
 
-    colorMap := make([]string, len(testString))
-    for i := 0; i < len(testString); i++ {
-        colorMap[i] = "white"
-    }
-
-    drawText(textView, colorMap)
+    drawText(textView, words, colorMap)
 
     currentChar := 0
     textView.Highlight("0")
     app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-        if currentChar >= len(testString) {
+        if currentChar >= len(words) {
             return event
         }
 
         if event.Key() == tcell.KeyBackspace || event.Key() == tcell.KeyBackspace2 {
-            currentChar -= 1
+            colorMap[currentChar] = "white"
 
+            currentChar -= 1
             if currentChar < 0 { currentChar = 0 }
 
             textView.Highlight(fmt.Sprintf("%d", currentChar))
+            drawText(textView, words, colorMap)
             return event
         }
 
-        if event.Rune() == rune(testString[currentChar]) {
+        if event.Rune() == rune(words[currentChar]) {
             colorMap[currentChar] = "green"
         } else {
             colorMap[currentChar] = "red"
         }
 
-        drawText(textView, colorMap)
+        drawText(textView, words, colorMap)
 
-        currentChar = (currentChar + 1) % len(testString)
+        currentChar += 1
+        if currentChar >= len(words) {
+            words, colorMap = newGame(characterPriority)
+            textView.Highlight("0")
+            currentChar = 0
+            drawText(textView, words, colorMap)
+        }
 
         textView.Highlight(fmt.Sprintf("%d", currentChar))
 
@@ -69,10 +74,48 @@ func main() {
 }
 
 
-func drawText(textView *tview.TextView, colorMap []string) {
+func newGame(characterPriority []dictionary.CharacterPriority) (string, []string){
+    numChars := 5
+    usedCharacters := characterPriority[:numChars]
+    priorityCharacter := usedCharacters[0]
+    wordCount := 10
+
+    words := ""
+    for i := 0; i < wordCount; i++ {
+        words += fmt.Sprintf("%s ", generateWord(usedCharacters, priorityCharacter))
+    }
+
+    colorMap := make([]string, len(words))
+    for i := 0; i < len(words); i++ {
+        colorMap[i] = "white"
+    }
+
+    return words, colorMap
+}
+
+
+func drawText(textView *tview.TextView, words string, colorMap []string) {
     textView.Clear()
 
-    for i, char := range testString {
+    for i, char := range words {
         fmt.Fprintf(textView, `["%d"][%s]%c[""]`, i, colorMap[i], char)
     }        
 } 
+
+
+func generateWord(chars []dictionary.CharacterPriority, priorityCharacter dictionary.CharacterPriority) string {
+    length := rand.Intn(3) + 3
+    priorityPosition := rand.Intn(length)
+
+    word := ""
+    for i := 0; i < length; i++ {
+        if i == priorityPosition {
+            word += string(priorityCharacter.Character)
+            continue
+        }
+
+        word += string(chars[rand.Intn(len(chars))].Character)
+    }
+
+    return word
+}
